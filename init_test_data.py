fd14fb4545430835361bd2d785e90b3d8a2c1444
KristinaImbir <pchelintseva2003@gmail.com>
2025-05-02 00:08:42 +0300

    fix(admin): жанры и тестовые данные

--- init_test_data.py
+++ init_test_data.py
@@ -88,6 +88,22 @@ if __name__ == "__main__":
         wiki_link="https://en.wikipedia.org/wiki/Steven_Spielberg"
     )
 
+    fincher_id = add_person(
+        name="David Fincher",
+        role="director",
+        birth_date="1962-08-28",
+        birth_place="Denver, Colorado",
+        wiki_link="https://en.wikipedia.org/wiki/David_Fincher"
+    )
+
+    jeunet_id = add_person(
+        name="Jean-Pierre Jeunet",
+        role="director",
+        birth_date="1953-09-03",
+        birth_place="Roanne, France",
+        wiki_link="https://en.wikipedia.org/wiki/Jean-Pierre_Jeunet"
+    )
+
     print("Добавляем актёров...")
     leo_id = add_person(
         name="Leonardo DiCaprio",
@@ -129,6 +145,39 @@ if __name__ == "__main__":
         wiki_link="https://en.wikipedia.org/wiki/Tom_Hanks"
     )
 
+    pitt_id = add_person(
+        name="Brad Pitt",
+        role="actor",
+        birth_date="1963-12-18",
+        birth_place="Shawnee, Oklahoma",
+        wiki_link="https://en.wikipedia.org/wiki/Brad_Pitt"
+    )
+
+    norton_id = add_person(
+        name="Edward Norton",
+        role="actor",
+        birth_date="1969-08-18",
+        birth_place="Boston, Massachusetts",
+        wiki_link="https://en.wikipedia.org/wiki/Edward_Norton"
+    )
+
+    bale_id = add_person(
+        name="Christian Bale",
+        role="actor",
+        birth_date="1974-01-30",
+        birth_place="Haverfordwest, Wales",
+        wiki_link="https://en.wikipedia.org/wiki/Christian_Bale"
+    )
+
+    ledger_id = add_person(
+        name="Heath Ledger",
+        role="actor",
+        birth_date="1979-04-04",
+        birth_place="Perth, Western Australia",
+        wiki_link="https://en.wikipedia.org/wiki/Heath_Ledger"
+    )
+
+
     print("Добавляем фильмы...")
     add_film(
         title="Inception",
@@ -138,7 +187,7 @@ if __name__ == "__main__":
         description="A thief who steals corporate secrets through dream-sharing technology",
         country="USA",
         duration=148,
-        genres=["sci-fi", "action"],
+        genres=["Фантастика", "Боевик"],
         budget=160000000,
         poster="https://i.pinimg.com/736x/6f/c5/a3/6fc5a3240f09d882606e55da4a58b2dd.jpg",
         video_path="/videos/inception.mp4",
@@ -153,7 +202,7 @@ if __name__ == "__main__":
         description="Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
         country="South Korea",
         duration=132,
-        genres=["drama", "thriller"],
+        genres=["Драма", "Триллер"],
         budget=11400000,
         poster="https://i.pinimg.com/736x/f3/17/a6/f317a612d86d1e324e4bd507cbf160aa.jpg",
         video_path="/videos/parasite.mp4",
@@ -168,7 +217,7 @@ if __name__ == "__main__":
         description="A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy.",
         country="Germany",
         duration=99,
-        genres=["comedy", "drama"],
+        genres=["Комедия", "Драма"],
         budget=25000000,
         poster="https://cdn.ananasposter.ru/image/cache/catalog/poster/pos23/23/68397-1000x830.jpg",
         video_path="/videos/grand_budapest.mp4",
@@ -183,7 +232,7 @@ if __name__ == "__main__":
         description="Feature adaptation of Frank Herbert's science fiction novel, about the son of a noble family entrusted with the protection of the most valuable asset and vital element in the galaxy.",
         country="USA",
         duration=155,
-        genres=["sci-fi", "adventure"],
+        genres=["Фантастика", "Приключения"],
         budget=165000000,
         poster="https://static.kinoafisha.info/k/movie_posters/1920x1080/upload/movie_posters/4/0/2/8355204/9ff0dfea8286428827af7478fdcd86a7.jpeg",
         video_path="/videos/dune.mp4",
@@ -198,11 +247,41 @@ if __name__ == "__main__":
         description="Following the Normandy Landings, a group of U.S. soldiers go behind enemy lines to retrieve a paratrooper whose brothers have been killed in action.",
         country="USA",
         duration=169,
-        genres=["war", "drama"],
+        genres=["Военный", "Драма"],
         budget=70000000,
         poster="https://posterspy.com/wp-content/uploads/2019/10/Saving_private_ryan_poster_NEW.jpg",
         video_path="/videos/saving_ryan.mp4",
         ratings=[8],
+    )
+
+    add_film(
+        title="Бойцовский клуб",
+        year=1999,
+        directors=[fincher_id],
+        actors=[pitt_id, norton_id],
+        description="Офисный работник страдает от бессонницы и встречает загадочного продавца мыла, с которым они основывают подпольный бойцовский клуб.",
+        country="США",
+        duration=139,
+        genres=["Драма", "Триллер"],
+        budget=63000000,
+        poster="https://posterspy.com/wp-content/uploads/2024/06/fight-club-e.jpg",
+        video_path="/videos/fight_club.mp4",
+        ratings=[9],
+    )
+
+    add_film(
+        title="Темный рыцарь",
+        year=2008,
+        directors=[nolan_id],
+        actors=[bale_id, ledger_id],
+        description="Бэтмен сталкивается с новым преступником по прозвищу Джокер, который бросает вызов всей системе правосудия в Готэме.",
+        country="США",
+        duration=152,
+        genres=["Боевик", "Криминал", "Драма"],
+        budget=185000000,
+        poster="https://posterspy.com/wp-content/uploads/2021/06/darkknight-12x18-72r.png",
+        video_path="/videos/dark_knight.mp4",
+        ratings=[9],
     )
 
     print("Добавляем администратора...")
